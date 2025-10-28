cb89d2764b2492f19cca4222bed7ae710318e4d4
MhayreeJhaine <mhayreejhaine@gmail.com>
2025-10-28 21:49:45 +0100

    updated navigation

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,47 +1,50 @@
-import { Link } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
   return (
     <header className="bg-white shadow-md p-4 flex justify-between items-center">
-      <h1 className="text-2xl font-bold text-blue-600">🎟️ Ticket Manager</h1>
+      <h1
+        className="text-2xl font-bold text-blue-600 cursor-pointer"
+        onClick={() => navigate("/dashboard")}
+      >
+        🎟️ Ticket Manager
+      </h1>
 
       <nav className="space-x-4">
-        <Link
-          to={"/homepage"}
-          className={({ isActive }) =>
-            `px-3 py-2 rounded ${
-              isActive
-                ? "bg-blue-500 text-white"
-                : "text-gray-700 hover:text-blue-600"
-            }`
-          }
+        <button
+          onClick={() => navigate("/")}
+          className={`px-3 py-2 rounded ${
+            pathname === "/"
+              ? "bg-blue-500 text-white"
+              : "text-gray-700 hover:text-blue-600"
+          }`}
         >
           Homepage
-        </Link>
-        <Link
-          to="/dashboard"
-          className={({ isActive }) =>
-            `px-3 py-2 rounded ${
-              isActive
-                ? "bg-blue-500 text-white"
-                : "text-gray-700 hover:text-blue-600"
-            }`
-          }
+        </button>
+
+        <button
+          onClick={() => navigate("/dashboard")}
+          className={`px-3 py-2 rounded ${
+            pathname === "/dashboard"
+              ? "bg-blue-500 text-white"
+              : "text-gray-700 hover:text-blue-600"
+          }`}
         >
           Dashboard
-        </Link>
-        <Link
-          to="/tickets"
-          className={({ isActive }) =>
-            `px-3 py-2 rounded ${
-              isActive
-                ? "bg-blue-500 text-white"
-                : "text-gray-700 hover:text-blue-600"
-            }`
-          }
+        </button>
+        <button
+          onClick={() => navigate("/tickets")}
+          className={`px-3 py-2 rounded ${
+            pathname === "/tickets"
+              ? "bg-blue-500 text-white"
+              : "text-gray-700 hover:text-blue-600"
+          }`}
         >
           Tickets
-        </Link>
+        </button>
       </nav>
     </header>
   );
